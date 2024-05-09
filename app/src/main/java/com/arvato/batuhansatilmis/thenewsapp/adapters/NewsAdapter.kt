d4e7bbc27d52f777d35d079be29f6abcd384d7d3
Satilmis <batuhan.satilmis@arvato-scs.com>
2024-05-09 17:23:59 +0300

    add: adapter for recyclerviews

--- app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/adapters/NewsAdapter.kt
@@ -4,7 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
-   inner class ArticleViewHolder(itemView : View) {
+   inner class ArticleViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
     }
 }
