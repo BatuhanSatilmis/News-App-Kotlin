0684106e1ad7bd9d01bc6d8d590d2a7663a0b7bf
batuhans <afrousse1998@gmail.com>
2024-05-12 22:53:35 +0300

    add: setupFavRecycler methods

--- app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/ui/fragments/FavouritesFragment.kt
+++ app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/ui/fragments/FavouritesFragment.kt
@@ -5,19 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arvato.batuhansatilmis.thenewsapp.R
+import com.arvato.batuhansatilmis.thenewsapp.adapters.NewsAdapter
+import com.arvato.batuhansatilmis.thenewsapp.databinding.FragmentFavouritesBinding
+import com.arvato.batuhansatilmis.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.arvato.batuhansatilmis.thenewsapp.ui.NewsViewModel
 
 
 class FavouritesFragment : Fragment() {
 
+    lateinit var newsViewModel : NewsViewModel
+    lateinit var newsAdapter : NewsAdapter
+    lateinit var binding : FragmentFavouritesBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFavouritesBinding.bind(view)
+
     }
-
+    private fun setupFavouritesRecycler(){
+        newsAdapter = NewsAdapter()
+        binding.recyclerFavourites.apply {
+            adapter = this@FavouritesFragment.newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 
 }
