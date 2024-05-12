56bf0afc5d58ea3052046bcd0714e190c666e0d4
batuhans <afrousse1998@gmail.com>
2024-05-12 23:16:15 +0300

    add: Searchfragment in to error message methods

--- app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/arvato/batuhansatilmis/thenewsapp/ui/fragments/SearchFragment.kt
@@ -6,31 +6,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.navArgs
 import com.arvato.batuhansatilmis.thenewsapp.R
+import com.arvato.batuhansatilmis.thenewsapp.adapters.NewsAdapter
 import com.arvato.batuhansatilmis.thenewsapp.databinding.FragmentSearchBinding
 import com.arvato.batuhansatilmis.thenewsapp.ui.NewsActivity
 import com.arvato.batuhansatilmis.thenewsapp.ui.NewsViewModel
 
 
-class SearchFragment : Fragment(R.layout.fragment_search) {
+class SearchFragment : Fragment() {
 
       private lateinit var newsViewModel: NewsViewModel
       private lateinit var binding: FragmentSearchBinding
-      val args: ArticleFragmentArgs by navArgs()
+      lateinit var newsAdapter: NewsAdapter
+      lateinit var retryButton: Button
+      lateinit var errorText: TextView
+      lateinit var itemSearchError: CardView
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSearchBinding.bind(view)
 
-        newsViewModel = (activity as NewsActivity).newsViewModel
-        val article = args.article
-
-        val  webViewClient = WebViewClient()
 
 
     }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar(){
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+    private fun showProgressBar(){
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+    private fun hideErrorMessage(){
+        binding.itemSearchError.errorText.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String){
+        binding.itemSearchError.errorText.visibility = View.VISIBLE
+        isError = true
+    }
+
+
+
 
 
 }
